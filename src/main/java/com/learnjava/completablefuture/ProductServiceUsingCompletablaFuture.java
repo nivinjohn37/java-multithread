2f44eedde1f7fc5abf791d76eb751d538577e5f0
nivinjohn <getnivin@gmail.com>
2024-04-19 23:46:35 +0530

    CompletableFutureExceptions

--- src/main/java/com/learnjava/completablefuture/ProductServiceUsingCompletablaFuture.java
+++ src/main/java/com/learnjava/completablefuture/ProductServiceUsingCompletablaFuture.java
@@ -1,11 +1,11 @@
 package com.learnjava.completablefuture;
 
-import com.learnjava.domain.Product;
-import com.learnjava.domain.ProductInfo;
-import com.learnjava.domain.Review;
+import com.learnjava.domain.*;
+import com.learnjava.service.InventoryService;
 import com.learnjava.service.ProductInfoService;
 import com.learnjava.service.ReviewService;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import static com.learnjava.util.CommonUtil.stopWatch;
@@ -14,13 +14,21 @@ import static com.learnjava.util.LoggerUtil.log;
 public class ProductServiceUsingCompletablaFuture {
     private ProductInfoService productInfoService;
     private ReviewService reviewService;
+    private InventoryService inventoryService;
 
     public ProductServiceUsingCompletablaFuture(ProductInfoService productInfoService, ReviewService reviewService) {
         this.productInfoService = productInfoService;
         this.reviewService = reviewService;
     }
 
+    public ProductServiceUsingCompletablaFuture(ProductInfoService productInfoService, ReviewService reviewService, InventoryService inventoryService) {
+        this.productInfoService = productInfoService;
+        this.reviewService = reviewService;
+        this.inventoryService = inventoryService;
+    }
+
     public Product retrieveProductDetails(String productId) {
+        stopWatch.reset();
         stopWatch.start();
 
         CompletableFuture<ProductInfo> cfProductInfo = CompletableFuture
@@ -46,6 +54,50 @@ public class ProductServiceUsingCompletablaFuture {
 
         return cfProductInfo
                 .thenCombine(cfReview, (productInfo, review) -> new Product(productId, productInfo, review));
+    }
+
+    public Product retrieveProductDetailsWithInventory(String productId) {
+        stopWatch.reset();
+        stopWatch.start();
+        CompletableFuture<ProductInfo> cfProductInfo = CompletableFuture
+                .supplyAsync(() -> productInfoService.retrieveProductInfo(productId))
+                .thenApply(productInfo -> {
+                    List<ProductOption> productOptionList = updateInventory_approach2(productInfo);
+                    productInfo.setProductOptions(productOptionList);
+                    return productInfo;
+                });
+        CompletableFuture<Review> cfReview = CompletableFuture
+                .supplyAsync(() -> reviewService.retrieveReviews(productId));
+
+        Product product = cfProductInfo
+                .thenCombine(cfReview, (productInfo, review) -> new Product(productId, productInfo, review)).join();
+        stopWatch.stop();
+        log("Total Time Taken : " + stopWatch.getTime());
+        return product;
+    }
+
+    private List<ProductOption> updateInventory(ProductInfo productInfo) {
+        return productInfo.getProductOptions()
+                .stream()
+                .map(productOption -> {
+                    Inventory inventory = inventoryService.addInventory(productOption);
+                    productOption.setInventory(inventory);
+                    return productOption;
+                }).toList();
+    }
+
+    private List<ProductOption> updateInventory_approach2(ProductInfo productInfo) {
+            List<CompletableFuture<ProductOption>> productOptionsListFuture = productInfo.getProductOptions()
+                .stream()
+                .map(productOption -> {
+                    return CompletableFuture.supplyAsync(() -> inventoryService.addInventory(productOption))
+                            .thenApply(inventory -> {
+                                productOption.setInventory(inventory);
+                                return productOption;
+                            });
+                }).toList();
+            return productOptionsListFuture.stream()
+                    .map(CompletableFuture::join).toList();
     }
 
     public static void main(String[] args) {
